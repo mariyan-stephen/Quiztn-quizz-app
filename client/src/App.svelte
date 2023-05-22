9727edeb95c52b2b24d6e8e8f4b88ec7fb599dfb
Mariyan-Stephen <tinkernthink@gmail.com>
2023-05-22 21:18:41 +0530

    Adding more svlete components

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,13 +2,16 @@
     import { Router, Route, Link } from "svelte-routing";
     import Home from "./components/Home.svelte";
     import About from "./components/About.svelte";
+    import Quiz from "./components/Quiz.svelte";	
 </script>
 
 <Router>
     <nav>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <Link to="/quiz">Quiz</Link>
     </nav>
     <Route path="/" component={Home} />
     <Route path="/about" component={About} />
+    <Route path="/quiz" component={Quiz} />
 </Router>
---
+++ client/src/components/Quiz.svelte
@@ -0,0 +1,22 @@
+<script>
+    let questions = [];
+
+    // Commenting out the API request until the server-side code is set up
+    /*
+    onMount(async () => {
+        const response = await fetch("/api/questions");
+        questions = await response.json();
+    });
+    */
+</script>
+
+<div>
+    {#each questions as question (question.id)}
+        <h2>{question.questionText}</h2>
+        <ul>
+            {#each question.answers as answer (answer.id)}
+                <li>{answer.answerText}</li>
+            {/each}
+        </ul>
+    {/each}
+</div>
