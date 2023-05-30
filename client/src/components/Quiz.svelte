24c2905597b0a9d7c2bb5405eee9ccf16094aaba
Mariyan-Stephen <tinkernthink@gmail.com>
2023-05-30 23:05:29 +0530

    Display the user's score after they finish answering all the questions.

--- client/src/components/Quiz.svelte
+++ client/src/components/Quiz.svelte
@@ -7,52 +7,52 @@
     }
 
     let questions = [];
+    let score = 0;
+    let quizFinished = false;
+
     fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple')
         .then(response => response.json())
         .then(data => {
             questions = data.results.map((question, index) => ({
                 id: index + 1,
-                questionText: question.question,
+                questionText: decodeHtml(question.question),
+                correctAnswer: decodeHtml(question.correct_answer),
                 answers: [
-                    ...question.incorrect_answers.map(answer => ({ answerText: answer, isCorrect: false })),
-                    { answerText: question.correct_answer, isCorrect: true }
+                    ...question.incorrect_answers.map(answer => decodeHtml(answer)),
+                    decodeHtml(question.correct_answer)
                 ].sort(() => Math.random() - 0.5) // to shuffle the answers
             }));
         });
 
     let currentQuestionIndex = 0;
-    let selectedAnswer = null;
-    let score = 0;
 
-    function selectAnswer(answer) {
-        selectedAnswer = answer;
-    }
-
-    function nextQuestion() {
-        if (selectedAnswer && selectedAnswer.isCorrect) {
+    function checkAnswer(answer) {
+        if (answer === questions[currentQuestionIndex].correctAnswer) {
             score++;
         }
-        selectedAnswer = null;
+
         if (currentQuestionIndex < questions.length - 1) {
             currentQuestionIndex++;
         } else {
-            // Quiz ended. Do something with the score.
-            console.log('Quiz ended. Your score: ', score);
+            quizFinished = true;
         }
     }
 </script>
 
-{#if questions.length > 0}
+{#if quizFinished}
+    <div>
+        <h2>Your score: {score}/{questions.length}</h2>
+    </div>
+{:else if questions.length > 0}
     <div>
         <h2>{questions[currentQuestionIndex].questionText}</h2>
         <ul>
-            {#each questions[currentQuestionIndex].answers as answer (answer.answerText)}
+            {#each questions[currentQuestionIndex].answers as answer (answer)}
                 <li>
-                    <button on:click={() => selectAnswer(answer)}>{answer.answerText}</button>
+                    <button on:click={() => checkAnswer(answer)}>{answer}</button>
                 </li>
             {/each}
         </ul>
-        <button on:click={nextQuestion}>Next Question</button>
     </div>
 {:else}
     <div>Loading...</div>
