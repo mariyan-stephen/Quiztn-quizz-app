32aaf875307ef9a0df23d5c3e00ae1edec9cb1ca
Mariyan-Stephen <tinkernthink@gmail.com>
2023-05-31 23:33:19 +0530

    Tried some styling and centered the options

--- client/src/components/Quiz.svelte
+++ client/src/components/Quiz.svelte
@@ -7,49 +7,92 @@
     }
 
     let questions = [];
-    let score = 0;
-    let quizFinished = false;
-
     fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple')
         .then(response => response.json())
         .then(data => {
             questions = data.results.map((question, index) => ({
                 id: index + 1,
-                questionText: decodeHtml(question.question),
-                correctAnswer: decodeHtml(question.correct_answer),
+                questionText: question.question,
                 answers: [
-                    ...question.incorrect_answers.map(answer => decodeHtml(answer)),
-                    decodeHtml(question.correct_answer)
+                    ...question.incorrect_answers.map(answer => ({ answerText: answer, isCorrect: false })),
+                    { answerText: question.correct_answer, isCorrect: true }
                 ].sort(() => Math.random() - 0.5) // to shuffle the answers
             }));
         });
 
     let currentQuestionIndex = 0;
+    let score = 0;
+    let quizFinished = false;
 
-    function checkAnswer(answer) {
-        if (answer === questions[currentQuestionIndex].correctAnswer) {
-            score++;
-        }
-
+    function nextQuestion() {
         if (currentQuestionIndex < questions.length - 1) {
             currentQuestionIndex++;
         } else {
             quizFinished = true;
         }
     }
+
+    function checkAnswer(answer) {
+        if (answer.isCorrect) {
+            score++;
+        }
+        nextQuestion();
+    }
 </script>
 
+<style>
+    .quiz-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        margin: auto;
+        padding: 2em;
+    }
+
+    .question {
+        margin-bottom: 1em;
+        text-align: center;
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+
+    .option {
+        background-color: #f0f0f0;
+        padding: 1em;
+        margin: 0.5em;
+        border-radius: 10px;
+        cursor: pointer;
+        transition: all 0.3s ease-in-out;
+    }
+
+    .option:hover {
+        background-color: #e0e0f0;
+    }
+
+    .score-display {
+        font-size: 2em;
+        font-weight: bold;
+        text-align: center;
+    }
+</style>
+
 {#if quizFinished}
-    <div>
+    <div class="score-display">
         <h2>Your score: {score}/{questions.length}</h2>
     </div>
 {:else if questions.length > 0}
-    <div>
-        <h2>{questions[currentQuestionIndex].questionText}</h2>
+    <div class="quiz-container">
+        <div class="question">
+            <h2>{decodeHtml(questions[currentQuestionIndex].questionText)}</h2>
+        </div>
         <ul>
-            {#each questions[currentQuestionIndex].answers as answer (answer)}
-                <li>
-                    <button on:click={() => checkAnswer(answer)}>{answer}</button>
+            {#each questions[currentQuestionIndex].answers as answer (answer.answerText)}
+                <li class="option" on:click={() => checkAnswer(answer)}>
+                    {decodeHtml(answer.answerText)}
                 </li>
             {/each}
         </ul>
