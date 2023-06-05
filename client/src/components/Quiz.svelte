8713de48158f09ac4fbcfdeaa6fb01a1a0a79292
Mariyan-Stephen <tinkernthink@gmail.com>
2023-06-05 20:52:22 +0530

    Tinkering with time and categories

--- client/src/components/Quiz.svelte
+++ client/src/components/Quiz.svelte
@@ -6,23 +6,31 @@
         return txt.value;
     }
 
+    let categories = [{ id: 9, name: "General Knowledge" }, /* add more categories as needed */];
+    let selectedCategory = categories[0].id;
+    
     let questions = [];
-    fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple')
-        .then(response => response.json())
-        .then(data => {
-            questions = data.results.map((question, index) => ({
-                id: index + 1,
-                questionText: question.question,
-                answers: [
-                    ...question.incorrect_answers.map(answer => ({ answerText: answer, isCorrect: false })),
-                    { answerText: question.correct_answer, isCorrect: true }
-                ].sort(() => Math.random() - 0.5) // to shuffle the answers
-            }));
-        });
-
     let currentQuestionIndex = 0;
     let score = 0;
     let quizFinished = false;
+    let timer = 30;
+
+    function getQuestions() {
+        fetch(`https://opentdb.com/api.php?amount=10&category=${selectedCategory}&difficulty=easy&type=multiple`)
+            .then(response => response.json())
+            .then(data => {
+                questions = data.results.map((question, index) => ({
+                    id: index + 1,
+                    questionText: question.question,
+                    answers: [
+                        ...question.incorrect_answers.map(answer => ({ answerText: answer, isCorrect: false })),
+                        { answerText: question.correct_answer, isCorrect: true }
+                    ].sort(() => Math.random() - 0.5) // to shuffle the answers
+                }));
+            });
+    }
+
+    getQuestions();
 
     function nextQuestion() {
         if (currentQuestionIndex < questions.length - 1) {
@@ -30,6 +38,7 @@
         } else {
             quizFinished = true;
         }
+        timer = 30;
     }
 
     function checkAnswer(answer) {
@@ -38,6 +47,18 @@
         }
         nextQuestion();
     }
+
+    function startTimer() {
+        let interval = setInterval(() => {
+            timer--;
+            if (timer === 0 || quizFinished) {
+                clearInterval(interval);
+                nextQuestion();
+            }
+        }, 1000); // 1000 milliseconds = 1 second
+    }
+
+    startTimer();
 </script>
 
 <style>
@@ -56,24 +77,20 @@
     }
 
     ul {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-gap: 20px;
         list-style: none;
         padding: 0;
-        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 20px;
     }
 
     .option {
         background-color: #f0f0f0;
         padding: 1em;
+        margin: 0.5em;
         border-radius: 10px;
         cursor: pointer;
         transition: all 0.3s ease-in-out;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
     }
 
     .option:hover {
@@ -88,6 +105,11 @@
 </style>
 
 <div class="component">
+    <select bind:value={selectedCategory} on:change={getQuestions}>
+        {#each categories as category}
+            <option value={category.id}>{category.name}</option>
+        {/each}
+    </select>
     {#if quizFinished}
         <div class="score-display">
             <h2>Your score: {score}/{questions.length}</h2>
@@ -104,6 +126,7 @@
                     </li>
                 {/each}
             </ul>
+            <div>Time Remaining: {timer}</div>
         </div>
     {:else}
         <div>Loading...</div>
