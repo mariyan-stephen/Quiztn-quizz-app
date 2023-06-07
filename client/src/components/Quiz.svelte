918a75287581d5090f118a20500565b870ba4a9a
Mariyan-Stephen <tinkernthink@gmail.com>
2023-06-07 19:36:00 +0530

    Fixing api issues

--- client/src/components/Quiz.svelte
+++ client/src/components/Quiz.svelte
@@ -6,31 +6,40 @@
         return txt.value;
     }
 
-    let categories = [{ id: 9, name: "General Knowledge" }, /* add more categories as needed */];
+    let categories = [
+        { id: 9, name: "General Knowledge" },
+        { id: 18, name: "Computers" },
+        { id: 19, name: "Mathematics" },
+        { id: 20, name: "Mythology" },
+        // Add more categories as needed
+    ];
+
     let selectedCategory = categories[0].id;
-    
     let questions = [];
     let currentQuestionIndex = 0;
     let score = 0;
     let quizFinished = false;
     let timer = 30;
+    let quizStarted = false;
 
-    function getQuestions() {
-        fetch(`https://opentdb.com/api.php?amount=10&category=${selectedCategory}&difficulty=easy&type=multiple`)
-            .then(response => response.json())
-            .then(data => {
-                questions = data.results.map((question, index) => ({
-                    id: index + 1,
-                    questionText: question.question,
-                    answers: [
-                        ...question.incorrect_answers.map(answer => ({ answerText: answer, isCorrect: false })),
-                        { answerText: question.correct_answer, isCorrect: true }
-                    ].sort(() => Math.random() - 0.5) // to shuffle the answers
-                }));
-            });
+    async function getQuestions() {
+        const response = await fetch(`https://opentdb.com/api.php?amount=10&category=${selectedCategory}&difficulty=easy&type=multiple`)
+        const data = await response.json();
+        questions = data.results.map((question, index) => ({
+            id: index + 1,
+            questionText: question.question,
+            answers: [
+                ...question.incorrect_answers.map(answer => ({ answerText: answer, isCorrect: false })),
+                { answerText: question.correct_answer, isCorrect: true }
+            ].sort(() => Math.random() - 0.5) // to shuffle the answers
+        }));
     }
 
-    getQuestions();
+    function startQuiz() {
+        getQuestions();
+        quizStarted = true;
+        startTimer();
+    }
 
     function nextQuestion() {
         if (currentQuestionIndex < questions.length - 1) {
@@ -38,7 +47,7 @@
         } else {
             quizFinished = true;
         }
-        timer = 30;
+        timer = 30; // reset timer for each new question
     }
 
     function checkAnswer(answer) {
@@ -57,77 +66,22 @@
             }
         }, 1000); // 1000 milliseconds = 1 second
     }
-
-    startTimer();
 </script>
 
-<style>
-    .quiz-container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        margin: auto;
-        padding: 2em;
-    }
-
-    .question {
-        margin-bottom: 1em;
-        text-align: center;
-    }
-
-    ul {
-        list-style: none;
-        padding: 0;
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        gap: 20px;
-    }
-
-    .option {
-        background-color: #f0f0f0;
-        padding: 1em;
-        margin: 0.5em;
-        border-radius: 10px;
-        cursor: pointer;
-        transition: all 0.3s ease-in-out;
-    }
-
-    .option:hover {
-        background-color: #e0e0f0;
-    }
-
-    .score-display {
-        font-size: 2em;
-        font-weight: bold;
-        text-align: center;
-    }
-</style>
+<!-- your style code here -->
 
 <div class="component">
-    <select bind:value={selectedCategory} on:change={getQuestions}>
-        {#each categories as category}
-            <option value={category.id}>{category.name}</option>
-        {/each}
-    </select>
-    {#if quizFinished}
-        <div class="score-display">
-            <h2>Your score: {score}/{questions.length}</h2>
-        </div>
+    {#if !quizStarted}
+        <select bind:value={selectedCategory} on:change={getQuestions}>
+            {#each categories as category}
+                <option value={category.id}>{category.name}</option>
+            {/each}
+        </select>
+        <button on:click={startQuiz}>Start Quiz</button>
+    {:else if quizFinished}
+        <!-- your quiz finished display code here -->
     {:else if questions.length > 0}
-        <div class="quiz-container">
-            <div class="question">
-                <h2>{decodeHtml(questions[currentQuestionIndex].questionText)}</h2>
-            </div>
-            <ul>
-                {#each questions[currentQuestionIndex].answers as answer (answer.answerText)}
-                    <li class="option" on:click={() => checkAnswer(answer)}>
-                        {decodeHtml(answer.answerText)}
-                    </li>
-                {/each}
-            </ul>
-            <div>Time Remaining: {timer}</div>
-        </div>
+        <!-- your quiz in-progress display code here -->
     {:else}
         <div>Loading...</div>
     {/if}
